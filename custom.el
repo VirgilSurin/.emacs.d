6779b267c2076e8724d2802fcf8d19e0442e2121
Virgil Surin <virgil.surin@student.umons.ac.be>
2020-11-08 13:52:10 +0100

    new emacs config

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(general helpful ivy-rich esup which-key virtualenv use-package spacemacs-theme rainbow-delimiters projectile lsp-ui lsp-java jedi helm-lsp flycheck ewal-doom-themes elpy doom-modeline counsel command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
